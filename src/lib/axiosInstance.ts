aeb9777711179ba9997be8412d2360a9fb96d73c
Shofi <shofiyullohkamil01@gmail.com>
2025-10-22 13:13:40 +0700

    fix: fixing axiosinstance

--- src/lib/axiosInstance.ts
+++ src/lib/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: process.env.PUBLIC_API_URL || 'https://learn.smktelkom-mlg.sch.id/kos/api',
+    baseURL: process.env.PUBLIC_API_URL,
     timeout: 10000,
     headers: {
         "Content-Type": "application/json",
